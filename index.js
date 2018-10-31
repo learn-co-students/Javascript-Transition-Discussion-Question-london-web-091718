b90895b5ab17b1561a099e69dc59a29c8c38b5be
Ed Putans <edputans@gmail.com>
2018-10-31 18:47:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
-// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
-//
-// function saveLincoln(interval) {
-//   if(isNaN(interval)) {
-//     interval = 10000
-//   }
-//   $('#foreground').fadeOut(interval);
-// }
 
-//
-// const zombiePic=document.querySelector("#foreground")
-// const text_area = document.querySelector('#interval')
-// const text_value=text_area.value
-// const button = document.querySelector('#save_lincoln')
-//
-// const sleeper=function(text_value){
-// for (let i=100; i>=0 ;i--){
-//
-//   zombiePic.style.opacity=i/100
-// }
-// }
-//
-//
-// button.addEventListener('click', sleeper)
-//
-// function sleeper(text_value)
-//
+let button = document.querySelector('#save_lincoln')
+const func = function() {
+let zombiePic = document.querySelector("#foreground")
+let text_area = document.querySelector('#interval')
+text_value=text_area.value
 
-const zombiePic=document.querySelector("#foreground")
-const text_area = document.querySelector('#interval')
-let text_value=text_area.value
-
-const button = document.querySelector('#save_lincoln')
 i=100
 
-function func(){
+setInterval(function(){
+  if(i>0) {
+    zombiePic.style.opacity = i/100
+    console.log(zombiePic.style.opacity)
+    i--
+  }
+},text_value/100)
 
-    if (i>0){
-      zombiePic.style.opacity=i/100
-      i--
-    }
-}
+text_area.addEventListener("inout",()=> {
+  console.log(text_area.value)
+  text_value = text_area.value
+})}
+// clear interval  - assign to variable
 
-
-button.addEventListener('click', function(text_value){
-
-  setInterval(func, text_value/100 )
-})
+button.addEventListener('click', func)
